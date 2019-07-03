f05ade2e1f8b0ebce5e2459ab97e22844ffe691e
George Schena <george@madetech.com>
2019-07-03 12:01:53 +0100

    passing tests

--- lib/use_case/view_teams.rb
+++ lib/use_case/view_teams.rb
@@ -5,8 +5,20 @@ module UseCase
     end
 
     def execute
+      projects = Gateway::GoogleSpreadsheet.new.all
 
+      data = {}
+      projects.each do |row|
+        data[row[0]] ||= {
+          client: row[0]
+        }
+        data[row[0]][:project] ||= []
+        data[row[0]][:project] << {
+          project_name: row[1],
+          person: row[2]
+        }
+      end
+      data.values
     end
-
   end
 end
--- spec/acceptance/schedulinator_spec.rb
+++ spec/acceptance/schedulinator_spec.rb
@@ -4,27 +4,6 @@ require 'gateway/google_spreadsheet'
 
 describe 'the schedule' do
 
-  let(:response) do
-    [["Amazon Web Services",
-      "LGSS Data Centre Migration",
-      "Ben Pirt",
-      "146.29",
-      "7",
-      "7",
-      "7",
-      "7",
-      "28"],
-     ["Amazon Web Services",
-      "LGSS Data Centre Migration",
-      "Robin Lacey",
-      "146.29",
-      "31.5",
-      "31.5",
-      "31.5",
-      "31.5",
-      "126"]]
-  end
-
   let(:google_spreadsheet_gateway) { Gateway::GoogleSpreadsheet.new }
   let(:view_teams) do
     UseCase::ViewTeams.new(google_spreadsheet_gateway: google_spreadsheet_gateway)
@@ -32,13 +11,57 @@ describe 'the schedule' do
 
   it 'views the spreadsheet' do
     response = view_teams.execute
-    expect(response[:teams]).to
-      eq(
-        [{:client=>"Amazon Web Services",
-          :project=>
-           [{:project_name=>"LGSS Data Centre Migration", :person=>"Ben Pirt"},
-            {:project_name=>"LGSS Data Centre Migration", :person=>"Robin Lacey"}]}
-        ]
+    expect(response).to eq(
+      [{:client=>"Amazon Web Services",
+        :project=>
+         [{:project_name=>"LGSS Data Centre Migration", :person=>"Ben Pirt"},
+          {:project_name=>"LGSS Data Centre Migration", :person=>"Robin Lacey"}]},
+       {:client=>"Department for Education",
+        :project=>
+         [{:project_name=>"Apply PO 41070009579", :person=>"Barry Anderson"},
+          {:project_name=>"Apply PO 41070009579", :person=>"Heidar Bernhardsson"},
+          {:project_name=>"Apply PO41070009580", :person=>"Kyle Chapman"},
+          {:project_name=>"Single Place for Education (SPFE)",
+           :person=>"Daniel Burnley"},
+          {:project_name=>"Single Place for Education (SPFE)",
+           :person=>"Robert McHugh"},
+          {:project_name=>"Single Place for Education (SPFE)",
+           :person=>"Wen Ting Wang"},
+          {:project_name=>"TVS Part II", :person=>"Adrian Clay"},
+          {:project_name=>"TVS Part II", :person=>"Dushan Despotovic"},
+          {:project_name=>"TVS Part II", :person=>"Omorinsola Fadoju"}]},
+       {:client=>"London Borough of Hackney",
+        :project=>
+         [{:project_name=>"Manage a Tenancy (ETRA)", :person=>"Ben Pirt"},
+          {:project_name=>"Manage a Tenancy (ETRA)", :person=>"Daniel Williams"},
+          {:project_name=>"Manage a Tenancy (ETRA)", :person=>"Jeff Pinkham"},
+          {:project_name=>"Manage a Tenancy (ETRA)", :person=>"Mark Sta Ana"},
+          {:project_name=>"Manage a Tenancy (ETRA)", :person=>"Maysa Kanoni"},
+          {:project_name=>"Manage Arrears: Support and Maintenance extension",
+           :person=>"Mark Sta Ana"}]},
+       {:client=>"Made Tech",
+        :project=>
+         [{:project_name=>"Chalet", :person=>"Cormac Brady"},
+          {:project_name=>"Chalet", :person=>"Csaba Gyorfi"},
+          {:project_name=>"Chalet", :person=>"Faissal Bensefia"}]},
+       {:client=>"Ministry of Justice",
+        :project=>
+         [{:project_name=>"Form Builder", :person=>"Elena Vilimaite"},
+          {:project_name=>"Form Builder", :person=>"Emile Swarts"},
+          {:project_name=>"Form Builder", :person=>"Tom Taylor"}]},
+       {:client=>"Smartwyre Inc",
+        :project=>
+         [{:project_name=>"Smartwyre", :person=>"Chris Parsons"},
+          {:project_name=>"Smartwyre", :person=>"Daniel O'Sullivan"},
+          {:project_name=>"Smartwyre", :person=>"David Capper"},
+          {:project_name=>"Smartwyre", :person=>"Emma Corbett"},
+          {:project_name=>"Smartwyre", :person=>"Jaseera Abubacker"},
+          {:project_name=>"Smartwyre", :person=>"Kelly Fragkogianni"},
+          {:project_name=>"Smartwyre", :person=>"Mark Sta Ana"},
+          {:project_name=>"Smartwyre", :person=>"Rebecca Fitzsimmons"},
+          {:project_name=>"Smartwyre", :person=>"Richard Foster"},
+          {:project_name=>"Smartwyre", :person=>"Steven Leighton"},
+          {:project_name=>"Smartwyre", :person=>"Tito Sarrionandia"}]}]
       )
   end
 end
