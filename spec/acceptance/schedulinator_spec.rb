30935867bf9bbc91f6446a0400288608cc8c166d
George Schena <george@madetech.com>
2019-07-03 15:47:36 +0100

    tests pass

--- spec/acceptance/schedulinator_spec.rb
+++ spec/acceptance/schedulinator_spec.rb
@@ -1,67 +1,44 @@
 # frozen_string_literal: true
 require 'use_case/view_teams'
 require 'gateway/google_spreadsheet'
+require 'vcr'
+require 'webmock/rspec'
 
 describe 'the schedule' do
-
-  let(:google_spreadsheet_gateway) { Gateway::GoogleSpreadsheet.new }
-  let(:view_teams) do
-    UseCase::ViewTeams.new(google_spreadsheet_gateway: google_spreadsheet_gateway)
+  VCR.configure do |config|
+    config.cassette_library_dir = "fixtures/vcr_cassettes"
+    config.hook_into :webmock
+    config.ignore_request {|request| request.uri == 'https://www.googleapis.com/oauth2/v4/token' }
   end
 
-  it 'views the spreadsheet' do
-    response = view_teams.execute
-    expect(response).to eq(
-      [{:client=>"Amazon Web Services",
-        :project=>
-         [{:project_name=>"LGSS Data Centre Migration", :person=>"Ben Pirt"},
-          {:project_name=>"LGSS Data Centre Migration", :person=>"Robin Lacey"}]},
-       {:client=>"Department for Education",
-        :project=>
-         [{:project_name=>"Apply PO 41070009579", :person=>"Barry Anderson"},
-          {:project_name=>"Apply PO 41070009579", :person=>"Heidar Bernhardsson"},
-          {:project_name=>"Apply PO41070009580", :person=>"Kyle Chapman"},
-          {:project_name=>"Single Place for Education (SPFE)",
-           :person=>"Daniel Burnley"},
-          {:project_name=>"Single Place for Education (SPFE)",
-           :person=>"Robert McHugh"},
-          {:project_name=>"Single Place for Education (SPFE)",
-           :person=>"Wen Ting Wang"},
-          {:project_name=>"TVS Part II", :person=>"Adrian Clay"},
-          {:project_name=>"TVS Part II", :person=>"Dushan Despotovic"},
-          {:project_name=>"TVS Part II", :person=>"Omorinsola Fadoju"}]},
-       {:client=>"London Borough of Hackney",
-        :project=>
-         [{:project_name=>"Manage a Tenancy (ETRA)", :person=>"Ben Pirt"},
-          {:project_name=>"Manage a Tenancy (ETRA)", :person=>"Daniel Williams"},
-          {:project_name=>"Manage a Tenancy (ETRA)", :person=>"Jeff Pinkham"},
-          {:project_name=>"Manage a Tenancy (ETRA)", :person=>"Mark Sta Ana"},
-          {:project_name=>"Manage a Tenancy (ETRA)", :person=>"Maysa Kanoni"},
-          {:project_name=>"Manage Arrears: Support and Maintenance extension",
-           :person=>"Mark Sta Ana"}]},
-       {:client=>"Made Tech",
-        :project=>
-         [{:project_name=>"Chalet", :person=>"Cormac Brady"},
-          {:project_name=>"Chalet", :person=>"Csaba Gyorfi"},
-          {:project_name=>"Chalet", :person=>"Faissal Bensefia"}]},
-       {:client=>"Ministry of Justice",
-        :project=>
-         [{:project_name=>"Form Builder", :person=>"Elena Vilimaite"},
-          {:project_name=>"Form Builder", :person=>"Emile Swarts"},
-          {:project_name=>"Form Builder", :person=>"Tom Taylor"}]},
-       {:client=>"Smartwyre Inc",
-        :project=>
-         [{:project_name=>"Smartwyre", :person=>"Chris Parsons"},
-          {:project_name=>"Smartwyre", :person=>"Daniel O'Sullivan"},
-          {:project_name=>"Smartwyre", :person=>"David Capper"},
-          {:project_name=>"Smartwyre", :person=>"Emma Corbett"},
-          {:project_name=>"Smartwyre", :person=>"Jaseera Abubacker"},
-          {:project_name=>"Smartwyre", :person=>"Kelly Fragkogianni"},
-          {:project_name=>"Smartwyre", :person=>"Mark Sta Ana"},
-          {:project_name=>"Smartwyre", :person=>"Rebecca Fitzsimmons"},
-          {:project_name=>"Smartwyre", :person=>"Richard Foster"},
-          {:project_name=>"Smartwyre", :person=>"Steven Leighton"},
-          {:project_name=>"Smartwyre", :person=>"Tito Sarrionandia"}]}]
+  let(:google_spreadsheet_gateway) { Gateway::GoogleSpreadsheet.new }
+  let(:view_teams) { UseCase::ViewTeams.new(google_spreadsheet_gateway: google_spreadsheet_gateway) }
+
+  it 'views the first team' do
+    VCR.use_cassette("response") do
+      response = view_teams.execute
+      expect(response.first).to eq(
+        {
+          :client =>"Bob Corp",
+          :project => [
+            {
+              :person=>"George",
+              :project_name=>"Project 1"
+            },
+            {
+              :person=>"Yusuf",
+              :project_name=>"Project 1"
+              }
+            ]
+        }
       )
+      end
+    end
+
+   it 'views the first team' do
+    VCR.use_cassette("response") do
+      response = view_teams.execute
+      expect(response.first[:client]).to eq("Bob Corp")
+    end
   end
 end
