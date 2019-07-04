986ff604f32247c190464c69e9b3acfa377248e0
Yusuf <47318342+yusufsheiqh@users.noreply.github.com>
2019-07-04 10:02:37 +0100

    Environment variable implementation (#10)
    * Add .env file
    
    * Add .env.sample file
    
    * Remove unnecessary file

--- Gemfile
+++ Gemfile
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
-
-gem 'sinatra', '~> 1.4', '>= 1.4.7'
-gem "google_drive"
-gem 'capybara', '~> 2.7', '>= 2.7.1'
-gem 'rspec', '~> 3.8'
+gem 'dotenv'
+gem 'google-api-client'
+gem 'rspec'
+gem 'sinatra'
 gem 'vcr'
 gem 'webmock'
--- spec/unit/gateway/google_sheets_teams.rb
+++ spec/unit/gateway/google_sheets_teams.rb
@@ -1,33 +1,33 @@
 # frozen_string_literal: true
+
 require 'gateway/google_spreadsheet'
 require 'vcr'
 
 describe Gateway::GoogleSpreadsheet do
-
   VCR.configure do |config|
-    config.cassette_library_dir = "fixtures/vcr_cassettes"
+    config.cassette_library_dir = 'fixtures/vcr_cassettes'
     config.hook_into :webmock
-    config.ignore_request {|request| request.uri == 'https://www.googleapis.com/oauth2/v4/token' }
+    config.ignore_request { |request| request.uri == 'https://www.googleapis.com/oauth2/v4/token' }
   end
 
   context 'getting the first clients data' do
-    VCR.use_cassette("response") do
+    VCR.use_cassette('response') do
       response = described_class.new.all
 
       it 'can show the first client' do
-        expect(response[0][0]).to eq("Bob Corp")
+        expect(response[0][0]).to eq('Bob Corp')
       end
 
       it 'can show the first active work stream' do
-          expect(response[0][1]).to eq("Project 1")
+        expect(response[0][1]).to eq('Project 1')
       end
 
       it 'can show the people working on the project' do
-        expect(response[0][2]).to eq("George")
+        expect(response[0][2]).to eq('George')
       end
 
       it 'can show the people working on the project' do
-        expect(response[1][2]).to eq("Yusuf")
+        expect(response[1][2]).to eq('Yusuf')
       end
     end
   end
