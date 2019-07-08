46699a3b7193940f4ba12eae43094e4cd52640aa
Maysa <maysa@madetech.com>
2019-07-08 15:39:35 +0100

    move script into file

---
+++ public/assets/js/main.js
@@ -0,0 +1,14 @@
+
+function search() {
+  var input = document.getElementById("Search");
+  var filter = input.value.toLowerCase();
+  var nodes = document.getElementsByClassName('target');
+
+  for (i = 0; i < nodes.length; i++) {
+    if (nodes[i].innerText.toLowerCase().includes(filter)) {
+      nodes[i].style.display = "block";
+    } else {
+      nodes[i].style.display = "none";
+    }
+  }
+};
