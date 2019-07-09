d32a71aa3b5e52e88d6df24be312d73db491169c
Yusuf <yusuf@madetech.com>
2019-07-09 14:05:36 +0100

    Refactor acceptance tests

--- spec/acceptance/schedulinator_spec.rb
+++ spec/acceptance/schedulinator_spec.rb
@@ -1,60 +1,80 @@
 # frozen_string_literal: true
 
-describe 'the schedule' do
-  VCR.configure do |config|
-    config.cassette_library_dir = 'fixtures/vcr_cassettes'
-    config.hook_into :webmock
-    config.ignore_request { |request| request.uri == 'https://www.googleapis.com/oauth2/v4/token' }
-  end
-
+describe 'the schedulinator' do
   let(:google_spreadsheet_gateway) { Gateway::GoogleSpreadsheet.new }
   let(:view_teams) { UseCase::ViewTeams.new(google_spreadsheet_gateway: google_spreadsheet_gateway) }
+  let(:response) { view_teams.execute }
+
+  it 'views all delivery teams' do
+    VCR.use_cassette('response') do
+      expect(response).to eq(
+        [
+          {
+            client: 'Client ONE',
+            project: [
+              {
+                project_name: 'Work Stream ONE',
+                start_date: '2019-05-02',
+                end_date: '2019-06-06',
+                person: ['Yusuf Sheikh'],
+                tech_lead: 'TRUE',
+                delivery_lead: 'TRUE',
+                client_partner: 'TRUE',
+                exec_sponsor: 'TRUE'
+              }
+            ]
+          },
+          {
+            client: 'Client TWO',
+            project: [
+              {
+                project_name: 'Work Stream ONE',
+                start_date: '2019-05-01',
+                end_date: '2019-10-31',
+                person: ['George Schena'],
+                tech_lead: 'TRUE',
+                delivery_lead: 'TRUE',
+                client_partner: 'TRUE',
+                exec_sponsor: 'TRUE'
+              },
+              {
+                project_name: 'Work Stream ONE',
+                start_date: '2019-05-01',
+                end_date: '2019-10-31',
+                person: ['Maysa Kanoni'],
+                tech_lead: 'FALSE',
+                delivery_lead: 'FALSE',
+                client_partner: 'FALSE',
+                exec_sponsor: 'FALSE'
+              },
+              {
+                project_name: 'Work Stream TWO',
+                start_date: '2019-07-15',
+                end_date: '2019-09-02',
+                person: ['Maysa Kanoni'],
+                tech_lead: 'TRUE',
+                delivery_lead: 'TRUE',
+                client_partner: 'TRUE',
+                exec_sponsor: 'TRUE'
+              }
+            ]
+          }
+        ]
+      )
+    end
+  end
 
   it 'views the first client' do
-    VCR.use_cassette('account_structure') do
-      response = view_teams.execute
-      expect(response.first[:client]).to eq('Amazon Web Services')
-    end
-  end
+    VCR.use_cassette('response') do
+      expect(response.first[:client]).to eq('Client ONE')
 
-  it 'views the first active work stream within the client' do
-    VCR.use_cassette('account_structure') do
-      response = view_teams.execute
-      expect(response.first[:project].first[:project_name]).to eq('LGSS Data Centre Migration')
-    end
-  end
+      expect(response.first[:project].first[:project_name]).to eq('Work Stream ONE')
+      expect(response.first[:project].first[:person]).to eq(['Yusuf Sheikh'])
 
-  it 'views the first person in the active work stream' do
-    VCR.use_cassette('account_structure') do
-      response = view_teams.execute
-      expect(response.first[:project].first[:person]).to eq('Ben Pirt')
-    end
-  end
-
-  it 'views the second person in the active work stream' do
-    VCR.use_cassette('account_structure') do
-      response = view_teams.execute
-      expect(response.first[:project][1][:person]).to eq('Robin Lacey')
-    end
-  end
-
-  it 'views the account structure of the first person in the first workstream' do
-    VCR.use_cassette('account_structure') do
-      response = view_teams.execute
-      expect(response.first[:project].first[:tech_lead]).to eq('FALSE')
-      expect(response.first[:project].first[:delivery_lead]).to eq('FALSE')
-      expect(response.first[:project].first[:client_partner]).to eq('FALSE')
-      expect(response.first[:project].first[:exec_sponsor]).to eq('FALSE')
-    end
-  end
-
-  it 'views the account structure of the second person in the first workstream' do
-    VCR.use_cassette('account_structure') do
-      response = view_teams.execute
-      expect(response.first[:project][1][:tech_lead]).to eq('FALSE')
-      expect(response.first[:project][1][:delivery_lead]).to eq('FALSE')
-      expect(response.first[:project][1][:client_partner]).to eq('FALSE')
-      expect(response.first[:project][1][:exec_sponsor]).to eq('FALSE')
+      expect(response.first[:project].first[:tech_lead]).to eq('TRUE')
+      expect(response.first[:project].first[:delivery_lead]).to eq('TRUE')
+      expect(response.first[:project].first[:client_partner]).to eq('TRUE')
+      expect(response.first[:project].first[:exec_sponsor]).to eq('TRUE')
     end
   end
 end
